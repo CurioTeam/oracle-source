8ad2d0b04ea13d0c7092c241129316c464017c76
joe-moyer <70274099+joe-moyer@users.noreply.github.com>
2020-10-02 20:22:07 +0200

    Update OracleSource.sol

--- contracts/OracleSource.sol
+++ contracts/OracleSource.sol
@@ -1,10 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+
 pragma solidity ^0.7.2;
 
 interface EACAggregatorProxy {
     function latestAnswer() external view returns (int256);
 }
 
-contract OracleSource {
+contract OracleProxy {
     EACAggregatorProxy immutable source;
     
     constructor(address _source) {
@@ -14,7 +16,8 @@ contract OracleSource {
     function peek() external view returns (bytes32 wut, bool ok) {
         int256 latestAnswer = source.latestAnswer();
         if (latestAnswer > 0) {
-            return (bytes32(latestAnswer), true);
+            uint256 answer = uint256(bytes32(latestAnswer));
+            return (bytes32(answer * 10 ** 13), true);
         } else {
             return (0, false);
         }
