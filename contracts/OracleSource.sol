126e52229d6aee5a5612fa9ea3d0066e2006463b
Vladimir Sushkov <volodya.sushkov@gmail.com>
2020-12-01 18:38:40 +0300

    Fix: correct oracle answer decimals and token price value

--- contracts/OracleSource.sol
+++ contracts/OracleSource.sol
@@ -8,16 +8,16 @@ interface EACAggregatorProxy {
 
 contract OracleProxy {
     EACAggregatorProxy immutable source;
-    
+
     constructor(address _source) {
         source = EACAggregatorProxy(_source);
     }
-    
+
     function peek() external view returns (bytes32 wut, bool ok) {
         int256 latestAnswer = source.latestAnswer();
         if (latestAnswer > 0) {
             uint256 answer = uint256(bytes32(latestAnswer));
-            return (bytes32(answer * 10 ** 13), true);
+            return (bytes32(answer * 1e10 / 1100000), true);
         } else {
             return (0, false);
         }
